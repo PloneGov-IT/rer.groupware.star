75f0e179d27aca1de61c46ed632715187aa7ddbc
keul <luca@keul.it>
2013-05-07 13:26:22 +0000

    import iniziale

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='rer.groupware.star',
+      version=version,
+      description="The RER Groupware suite",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Plone :: 4.2",
+        "Programming Language :: Python",
+        ],
+      keywords='plone plonegov groupware rer',
+      author='RedTurtle Technology',
+      author_email='sviluppoplone@redturtle.it',
+      url='http://svn.plone.org/svn/collective/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['rer', 'rer.groupware'],
+      include_package_data=True,
+      packages=find_packages('src', exclude=['ez_setup']),
+      package_dir={'': 'src'},
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
